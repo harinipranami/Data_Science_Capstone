20fdf056f73bbba52607a100941fe128c0422298
Harini <harinipranami@gmail.com>
2019-09-12 16:12:30 +1000

    Adds a python script that prints a greeting message for Capstone Assignment1.

---
+++ Assignment1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#This notebook will be mainly used for the capstone project
+
+import pandas as pd
+import numpy as np
+
+print('Hello Capstone Project Course!')
+
